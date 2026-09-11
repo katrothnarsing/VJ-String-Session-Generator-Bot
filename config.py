55ce27d72df2453f77ad58f78d6a4d22b40fb1b1
katrothnarsing <katrothnarsing91@gmail.com>
2026-09-11 21:08:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 from os import environ
 
 # Telegram Account Api Id And Api Hash
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "35280450"))
+API_HASH = environ.get("API_HASH", "6f7ae5fff983b4cd870de5483626c623")
 
 # Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
-OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
+OWNER_ID = int(environ.get("OWNER_ID", "8735651766")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
 F_SUB = environ.get("F_SUB", "")
